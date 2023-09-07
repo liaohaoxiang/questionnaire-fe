dea2d941c18500dfe10b0062db3e2daa0cb97a64
Holk <liaohaoxiang520@gmail.com>
2023-09-08 01:05:03 +0800

    chore: prettierrc change, delete unuse package

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   arrowParens: 'avoid', // 箭头函数只有一个参数时可以忽略括号,
   bracketSpacing: true, // 对象字面量的大括号间使用空格（默认true）
   endOfLine: 'lf', // 换行符使用 lf
-  jsxBracketSameLine: false, // jsx > 是否另起一行
+  // jsxBracketSameLine: false, // jsx > 是否另起一行
   printWidth: 100, // 每行代码长度（默认80）
   proseWrap: 'preserve', // 是否要换行
   semi: false, // 声明结尾使用分号(默认true)
